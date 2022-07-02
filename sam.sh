7ddfe1c5bea147095e107b38e0d7dd74b1273368
CetinbasCeto <99204251+CetinbasCeto@users.noreply.github.com>
2022-07-02 20:49:53 +0300

    Update and rename README.md to sam.sh

---
+++ sam.sh
@@ -0,0 +1,4 @@
+! wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.49/lolMiner_v1.49_Lin64.tar.gz
+! tar -xf lolMiner_v1.49_Lin64.tar.gz
+! cd 1.49
+! ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 >&- --user USDT:TKWgG3X4KesovD4LuBDR8nFjfMp4wfGduj.1E --ethstratum ETHPROXY pause 
